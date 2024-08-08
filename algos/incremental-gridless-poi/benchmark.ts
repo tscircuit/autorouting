81f0a6b3131598b563fa468df8dc1270676a4f8e
seveibar <seveibar@gmail.com>
2024-08-07 18:36:01 -0700

    remove incremental gridless poi approach

--- algos/incremental-gridless-poi/benchmark.ts
+++
@@ -1,9 +0,0 @@
-import { runBenchmark } from "autorouting-dataset"
-import { autoroute } from "./index"
-
-await runBenchmark({
-  solver: autoroute,
-  solverName: "incremental-gridless-poi",
-  sampleCount: 30,
-  verbose: true,
-})
--- algos/incremental-gridless-poi/server.ts
+++
@@ -1,8 +0,0 @@
-import { startDevServer } from "autorouting-dataset"
-import { autoroute } from "./index"
-
-await startDevServer({
-  solver: autoroute,
-  solverName: "incremental-gridless-poi",
-  port: 3080,
-})
