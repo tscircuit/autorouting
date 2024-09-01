86a1c0a4a093eec91835b08ff25e31a26931f22b
seveibar <seveibar@gmail.com>
2024-09-01 10:31:34 -0700

    minor refactor to break out getObstaclesFromCircuitJson

--- module/index.ts
+++ module/index.ts
@@ -3,3 +3,9 @@ export { runBenchmark } from "./lib/benchmark/run-benchmark"
 export { getSimpleRouteJson } from "./lib/solver-utils/getSimpleRouteJson"
 export * from "./lib/types"
 export * from "./lib/solver-utils/SimpleRouteJson"
+export * from "./lib/solver-utils/getObstaclesFromCircuitJson"
+export * from "./lib/solver-utils/getSimpleRouteJson"
+
+// COMPAT
+import { getObstaclesFromCircuitJson } from "./lib/solver-utils/getObstaclesFromCircuitJson"
+export const getObstaclesFromSoup = getObstaclesFromCircuitJson
