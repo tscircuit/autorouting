707c2ea42f61f825f92b29276243a5f5ebf7e4bf
seveibar <seveibar@gmail.com>
2024-09-13 21:39:07 -0700

    introduce alternative goal box functions

---
+++ module/lib/solver-utils/ConnectionWithAlternatives.ts
@@ -0,0 +1,12 @@
+import type { Obstacle } from "../types"
+import type { PointWithLayer, SimpleRouteConnection } from "./SimpleRouteJson"
+
+/**
+ * A connection with goal alternatives is a 2 point connection that has a single
+ * start point, but many possible "goal boxes". Touching any goal box will connect
+ * you to the network and finish the connection
+ */
+export interface ConnectionWithGoalAlternatives extends SimpleRouteConnection {
+  startPoint: PointWithLayer
+  goalBoxes: Obstacle[]
+}
