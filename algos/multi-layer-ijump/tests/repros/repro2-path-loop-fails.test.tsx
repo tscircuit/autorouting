51b97df5366433be9aa1add76802b028f7a5a611
seveibar <seveibar@gmail.com>
2024-10-25 21:44:35 -0700

    wip

--- algos/multi-layer-ijump/tests/repros/repro2-path-loop-fails.test.tsx
+++ algos/multi-layer-ijump/tests/repros/repro2-path-loop-fails.test.tsx
@@ -1,7 +1,7 @@
 import { MultilayerIjump } from "algos/multi-layer-ijump/MultilayerIjump"
 import { test, expect } from "bun:test"
 
-test("repro2-path-loop-fails", () => {
+test("repro2 path-loop-fails on single-trace sample 9", () => {
   // TODO load in single-trace sample 9, run it with both remove path enabled
   // and disabled, and compare the results
 })
