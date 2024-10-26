32ff5a36a27f0cfd64d6ae72eeacdde6318aaba8
seveibar <seveibar@gmail.com>
2024-10-25 21:43:35 -0700

    repro2 path loop fails

---
+++ algos/multi-layer-ijump/tests/repros/repro2-path-loop-fails.test.tsx
@@ -0,0 +1,7 @@
+import { MultilayerIjump } from "algos/multi-layer-ijump/MultilayerIjump"
+import { test, expect } from "bun:test"
+
+test("repro2-path-loop-fails", () => {
+  // TODO load in single-trace sample 9, run it with both remove path enabled
+  // and disabled, and compare the results
+})
